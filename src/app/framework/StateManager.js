fa84bd383e5b65c3a7c2fb4af54e0dc4cc43959d
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-08 15:00:26 -0300

    gerenciador de estado nextjs

--- src/app/framework/StateManager.js
+++ src/app/framework/StateManager.js
@@ -2,4 +2,11 @@ class StateManager {
     constructor(){
         this.observers = new Map();
     }
+
+    subscribe(key, callback){
+        if(!this.observers.has(key)){
+            this.observers.set(key, []);
+        }
+        this.observers.get(key).push(callback);
+    }
 }

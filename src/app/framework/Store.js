73b601b9b0c2aa3a1f2d06c8ce3fd5c4d84016bb
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-14 14:54:53 -0300

    gerenciador de estado nextjs

--- src/app/framework/Store.js
+++ src/app/framework/Store.js
@@ -14,6 +14,14 @@ class Store {
     getState(key){
         this.state.get(key)
     }
+
+    loadFromLocalStorage(){
+        for(let i = 0; i < localStorage.length; i++){
+            const key = localStorage.key(i);
+            const value = JSON.parse(localStorage.getItem(key));
+            this.state.set(key, value);
+        }
+    }
 }
 
 export const store = new Store();

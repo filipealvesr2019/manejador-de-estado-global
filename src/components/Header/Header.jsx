04cb7fc1f92cecca24ab413ba9a7b69a9600e35e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-23 14:46:08 -0300

    feat: cria useGlobalState

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 
 "use client"
 import CartIcon from "../CartIcon";
+import styles from './Header.module.css'
 
 export default function Header(){
     return (
-        <header>
+        <header className={styles.container}>
+            <div className={styles.div}>
             <span>Logo</span>
             <div>
                 <CartIcon />
+            </div>
+
             </div>
         </header>
     )

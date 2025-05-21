9b06417ca8207e145905fd3c22152bd8f651010c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-21 14:38:54 -0300

    add

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,10 +1,19 @@
 export default function ProductList(){
+    const [produtos, setProdutos] = useGlobalState("produtos");
 
     useEffect(() => {
         fecth("http://localhost:3000/api/products")
         .then(res => res.json())
-        .then()
-    }, [])
+        .then(setProdutos)
+    }, []);
+
+    const adicionarAoCarrinho = async (produtoId) => {
+        await fetch("http://localhost:3000/carrinho", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({ produtoId, quantidade: 1 })
+        })
+    }
     return (
         <div>
         

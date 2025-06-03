0066fcfd6fc0a3fbc60024b2a0d4e3172244dd0e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-03 15:39:20 -0300

    add

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -6,19 +6,20 @@ export default function ProductList(){
     const [produtos, setProdutos] = useGlobalState("produtos");
 
     useEffect(() => {
-        fetch("http://localhost:3000/produtos")
+        fetch("http://localhost:3001/produtos")
         .then(res => res.json())
         .then(setProdutos)
+        .catch(err => console.log(err));
     }, []);
 
     const adicionarAoCarrinho = async (produtoId) => {
-        await fetch("http://localhost:3000/carrinho", {
+        await fetch("http://localhost:3001/carrinho", {
             method: "POST",
             headers: { "Content-Type": "application/json"},
             body: JSON.stringify({ produtoId, quantidade: 1 })
         });
 
-        const res = await fetch("http://localhost:3000/carrinho");
+        const res = await fetch("http://localhost:3001/carrinho");
         const data = await res.json();
         const total = data.produtos.reduce((acc, p) => acc + p.quantidade, 0);
         const [_, setCarrinhoCount] = useGlobalState("carrinho");
